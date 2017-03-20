7282c70f0b198c2550ab11e94cc7e32ba2cd00b9
Chris NeJame <cnejame@truveris.com>
2017-03-20 10:39:26 -0400

    version 0.7.3

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ project = u'contextional'
 copyright = u'2017, Chris NeJame'
 author = u'Chris NeJame'
 
-release = u'0.7.2'
+release = u'0.7.3'
 
 language = None
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-__VERSION__ = "0.7.2"
+__VERSION__ = "0.7.3"
 
 setup(
     name="contextional",
