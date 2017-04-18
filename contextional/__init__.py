df4f630ee316689d6dfc475d3d544a77bdc8e060
Chris NeJame <cnejame@truveris.com>
2017-04-18 11:43:53 -0400

    should use __all__ and only import what's necessary

--- contextional/__init__.py
+++ contextional/__init__.py
@@ -1,3 +1,8 @@
 from __future__ import absolute_import
 
-from contextional.contextional import *
+from contextional.contextional import GroupContextManager
+
+
+__all__ = [
+    "GroupContextManager",
+]
